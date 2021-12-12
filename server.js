d026c5ffb1aae561c40100eb50a4649bba3bfaec
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-12-12 17:21:46 -0500

    added sessions npm

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const express = require("express");
 const routes = require("./routes");
 const sequelize = require("./config/connection");
 const path = require("path");
+const session = require("express-session");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+  secret: "Super Secret Number",
+  resave: false,
+  saveUninitialized: false, //only have saved when we use like a login
+};
+
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
