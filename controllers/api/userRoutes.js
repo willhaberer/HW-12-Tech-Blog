4df22b963dba0a353071dc504180cf04282b5288
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-12-15 16:37:21 -0500

    fixed user import to user routes and tested get and post routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const sequelize = require("../../config/connection");
-const { User } = require("../../models");
+const User = require("../../models/User.js");
 
 router.get("/", async (req, res) => {
   try {
