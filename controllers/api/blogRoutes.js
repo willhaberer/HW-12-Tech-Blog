824cf0842b5f51fc3f49ac3a737f09fc552a9e33
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-12-15 16:41:36 -0500

    created post route for new blogs

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -10,3 +10,15 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.post("/", async (req, res) => {
+  try {
+    const blogData = await Blog.create(req.body);
+    res.status(200).json(blogData);
+    console.log(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
