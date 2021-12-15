139628d465d6a94ec7330d89117f064734ba4fc5
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-12-15 16:35:38 -0500

    imported user model into user routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const sequelize = require("../../config/connection");
+const { User } = require("../../models");
 
 router.get("/", async (req, res) => {
   try {
