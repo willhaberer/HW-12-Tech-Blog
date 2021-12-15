8f6d1cab3c9139ecb827a82f43f6d875081f7414
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-12-15 16:40:46 -0500

    created get route for blogs

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,3 +1,12 @@
 const router = require("express").Router();
 const sequelize = require("../../config/connection");
 const Blog = require("../../models/Blog.js");
+
+router.get("/", async (req, res) => {
+  try {
+    const blogData = await Blog.findAll();
+    res.status(200).json(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
