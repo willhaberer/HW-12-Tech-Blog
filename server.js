a3f0c06b530e60f729b5e4341d6fe4682376f6a8
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-12-16 10:03:52 -0500

    server js sequelize store

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
 const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,7 +13,11 @@ const PORT = process.env.PORT || 3001;
 const sess = {
   secret: "Super Secret Number",
   resave: false,
+  cookie: {},
   saveUninitialized: false, //only have saved when we use like a login
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
