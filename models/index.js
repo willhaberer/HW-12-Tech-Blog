af99cf280bd903833c595753fee6b20ac7531e97
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-12-16 09:27:12 -0500

    built out models index

--- models/index.js
+++ models/index.js
@@ -1,3 +1,18 @@
 const User = require("./User");
 const Blog = require("./Blog");
 const Comment = require("./Comment");
+
+User.hasMany(Blog, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Blog.hasMany(Comment, {
+  foreignKey: "blog_id",
+  onDelete: "CASCADE",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
